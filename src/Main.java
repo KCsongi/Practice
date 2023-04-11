6bfa97e6f72973d9ed0b8d2ed325b57534125c11
Csongi <csongorkiss94@gmail.com>
2023-04-11 19:44:28 +0200

    commitom

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,8 @@ public class Main {
                             26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
                             51, 52, 53, 55, 56, 57, 58, 59, 60,61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
                             76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100};
+        int[] oneToTen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        findPairs(oneToTen, 10);
 /*        findDuplicatedNumber(h);
         smallestAndLargestNum(h);*/
 /*        duplicate("macska");
@@ -226,7 +228,15 @@ public class Main {
 
     //16. Feladat
     // Find all pairs of integer arrays whose sum is equal to a given number
-
+    public static void findPairs(int[] a , int num) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i+1; j < a.length; j++) {
+                if((a[i] + a[j]) == num) {
+                    System.out.println("A párok: " + a[i] + " - " + a[j]);
+                }
+            }
+        }
+    }
 
     //17. Feladat
     // Find duplicate numbers in an array if it contains multiple duplicates
