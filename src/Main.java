14fad25df158b4da5578db332d4ff2f4547b6149
Csongi <csongorkiss94@gmail.com>
2023-04-11 19:39:50 +0200

    commitom

--- src/Main.java
+++ src/Main.java
@@ -17,14 +17,20 @@ public class Main {
         reverseWords("Ez teljesen jó");
         System.out.println(palindrome("kosarasok"));*/
         int[] h = {5,10,15,5,6,10,1,56,89,0,2345,89};
+        int[] oneToHundred = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+                            26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+                            51, 52, 53, 55, 56, 57, 58, 59, 60,61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
+                            76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100};
 /*        findDuplicatedNumber(h);
         smallestAndLargestNum(h);*/
-        duplicate("macska");
+/*        duplicate("macska");
         duplicatedChar("macska aki okos");
-        removeDuplicates(h);
+        removeDuplicates(h);*/
+
+        missingNumber(oneToHundred);
 
     }
-    //1.feladat
+    //1. Feladat
     //Print duplicate characters from String
     public static void duplicatedChar(String myString) {
         Set<Character> mySet = new HashSet<>();
@@ -38,7 +44,7 @@ public class Main {
         }
         System.out.println(mySet);
     }
-    //2.feladat
+    //2. Feladat
     //Check if two Strings are anagrams of each other
     public static void anagrams(String first, String second) {
         //Beletöltöm őket egy char tömbbe
@@ -49,7 +55,7 @@ public class Main {
         Arrays.sort(c2);
         System.out.println(Arrays.equals(c1, c2));
     }
-    //3.feladat
+    //3. Feladat
     // Print the first non-repeated character from String
     public static void nonRepeatedCharacter(String myString) {
         for (char i : myString.toCharArray()) {
@@ -59,12 +65,12 @@ public class Main {
             }
         }
     }
-    //4.feladat
+    //4. Feladat
     public static void recursive(String myString) {
 
 
     }
-    //5.feladat
+    //5. Feladat
     //Check if a String contains only digits
     public static boolean onlyDigits(String myString) {
         for (char i : myString.toCharArray()) {
@@ -76,7 +82,7 @@ public class Main {
         System.out.println("Ez csak betű");
         return false;
     }
-    //6.feladat
+    //6. Feladat
     //Find duplicate characters in a String
     public static void duplicate(String myString) {
         Set<Character> a = new HashSet<>();
@@ -87,7 +93,7 @@ public class Main {
         }
         System.out.println("A duplikátumok: " + a);
     }
-    //7.FELADAT
+    //7. Feladat
     // Count many vowels and consonants in a given String
     public static void vowelCounter(String myString) {
         //a, á, e, é, i, í, o, ó, ö, ő, u, ú, ü, ű.
@@ -102,7 +108,7 @@ public class Main {
         }
         System.out.println("Ennyi magánhangzó van benne: " + vowels);
     }
-    //8.Feladat
+    //8. Feladat
     // Count the occurrence of a given character in String
     public static void characterCounter(String myString, char myChar) {
         int counter = 0;
@@ -113,7 +119,7 @@ public class Main {
         }
         System.out.println("A " + myChar + " karakter " + counter + " alkalommal szerepel");
     }
-    //9.Feladat
+    //9. Feladat
     // Find all permutations of String
     public static void permutations(String myString) {
         //ABC
@@ -138,7 +144,7 @@ public class Main {
             printPermutn(ros, ans+ch);
         }
     }
-    //10.Feladat
+    //10. Feladat
     //Reverse words in a given sentence without using any library method
     public static void reverseWords(String myString) {
         String[] h = myString.split(" ");
@@ -148,10 +154,10 @@ public class Main {
         }
         System.out.println(help);
     }
-    //11.Feladat
+    //11. Feladat
     //Check if two String is a rotation of each other
 
-    //12.Feladat
+    //12. Feladat
     //Check if the given String is Palindrome
     public static boolean palindrome(String myString) {
         char[] a = myString.toCharArray();
@@ -166,6 +172,29 @@ public class Main {
         }
         return false;
     }
+
+    //13. Feladat
+    //Find a missing number in a given integer array of 1 to 100
+    public static void missingNumber(int[] myArray) {
+        ArrayList<Integer> a = new ArrayList<>();
+        int sumA = 0;
+        for (int i = 1; i <= 100; i++) {
+            a.add(i);
+            sumA += i;
+        }
+        int sumB = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            sumB += myArray[i];
+        }
+        int calc = sumA - sumB;
+        if (calc == 0) {
+            System.out.println("Minden szám megvan");
+        } else {
+            System.out.println("A hiányzó szám: " + calc);
+        }
+
+    }
+
     // 14. Feladat
     //Find the duplicate number on a given integer array
     public static void findDuplicatedNumber(int[] myArray) {
@@ -199,12 +228,11 @@ public class Main {
     // Find all pairs of integer arrays whose sum is equal to a given number
 
 
-    //17.Feladat
+    //17. Feladat
     // Find duplicate numbers in an array if it contains multiple duplicates
 
-    //18.feladat
+    //18. Feladat
     //Remove duplicates from the given array in Java
-
     public static void removeDuplicates(int[] myArray) {
         Set<Integer> mySet = new HashSet<>();
         for (int i = 0; i < myArray.length; i++) {
